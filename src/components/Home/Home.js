94758d04dd0756f55a41438098dfb77d0bc928c1
sunbabu2022 <sohelrana157631@gmail.com>
2023-02-17 23:28:19 +0600

    my first ever commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <h1>HOME: React-router Tutorial</h1>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <h3>Welcome to our website</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, excepturi quia. Expedita accusantium ut eius deleniti cum iusto ad consequuntur eveniet asperiores porro. Beatae quisquam qui aperiam sunt est excepturi nemo saepe ratione itaque vel minima eum fuga mollitia dolore dolorum, repellat possimus delectus perspiciatis! Temporibus neque illum consectetur, exercitationem odio expedita enim asperiores a aut sint, incidunt, veniam dolorem. Laborum similique dolorem tenetur dignissimos suscipit explicabo perspiciatis totam, alias est ad provident. Mollitia ipsa, libero sit in adipisci quasi. Ad error ipsum provident molestias nostrum praesentium, aperiam voluptates aliquid enim accusamus veniam sit, dicta rem, quas fugit corrupti vel?</p>
+            
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import Header from '../Header/Header';
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Error 404!! : Not Found</h1>
+        </div>
+    );
+};
+
+export default NotFound;
