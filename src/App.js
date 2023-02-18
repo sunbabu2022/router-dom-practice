aac805c62acf213d1476ab6110a8592545c4df5a
sunbabu2022 <sohelrana157631@gmail.com>
2023-02-19 00:38:33 +0600

    home link added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route index element={<Home/>} />
           <Route path="/" element={<Home/>} />
-          <Route path ="/home" element={<Home/>}/>
+          <Route path ="/home"  element={<Home/>}/>
           <Route path ="/about" element={<About/>}/>
           <Route path ="/contact" element={<Contact/>}/>
           <Route path ="*" element={<NotFound/>}/>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+
+                 <a href="/home">Home</a>
+                 <a href="/about">About</a>
+                 <a href="/contact">Contact</a>
         </div>
     );
 };
